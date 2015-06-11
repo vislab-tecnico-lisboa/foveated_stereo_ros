d5b29d09f73ac98dbae3377eeee5d64b8214c893
rmhpf <ruihortafigueiredo@gmail.com>
2015-06-11 19:45:07 +0100

    backup

---
+++ src/Gaze.h
@@ -0,0 +1,19 @@
+#ifndef GAZE_H
+#define GAZE_H
+
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+class Gaze
+{
+public:
+    Gaze();
+};
+
+#endif // GAZE_H
